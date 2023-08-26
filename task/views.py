fd008cda66e01444628d4646bba18e6bedfbfdd4
Oscar Daniel Villegas Martinez <oscar_martinezt@hotmail.com>
2023-08-26 02:30:30 -0600

    V1.1 Login-Bst

--- task/views.py
+++ task/views.py
@@ -78,14 +78,14 @@ def signout(request):
 def signin(request):
     if request.method == 'GET':
         return render(request, 'signin.html', {
-            'form': AuthenticationForm()
+            ##'form': AuthenticationForm()
         })
     else:
         user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
         if user is None:
             return render(request, 'signin.html', {
-            'form': AuthenticationForm(),
-            'error': 'Username or password is incorrect'
+            'error': 'Username or password is incorrect',
+            ##'form': AuthenticationForm(),
         })
         else:
             login(request, user)
